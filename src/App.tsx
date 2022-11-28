c829d3697483ec175105ea034462c0f8df3118a9
Denis Homoki <homoki.denisth@gmail.com>
2022-11-28 20:06:31 +0200

    added main section

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Navbar from "./components/Navbar";
 import ParticleBackground from "./components/ParticleBackground";
 import Container from "@mui/material/Container";
 import Box from "@mui/material/Box";
+import Main from "./components/Main";
 import Test from "./components/Test";
 
 function App() {
@@ -14,6 +15,9 @@ function App() {
         <Container maxWidth="lg" id="container">
           <Box sx={{ bgcolor: "transparent", height: "100vh" }}>
             <Navbar />
+            <Box className="main-container">
+              <Main />
+            </Box>
           </Box>
         </Container>
       </React.Fragment>
---
+++ src/components/Main.jsx
@@ -0,0 +1,41 @@
+import * as React from "react";
+import { styled } from "@mui/material/styles";
+import Grid from "@mui/material/Grid";
+import Paper from "@mui/material/Paper";
+import Box from "@mui/material/Box";
+import video from "../assets/computerra.mp4";
+
+export default function Main() {
+  return (
+    <Box className="main">
+      <Grid container rowSpacing={8} columns={{ xs: 4, md: 12 }} spacing={8}>
+        <Grid
+          item
+          xs={6}
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+          }}
+        >
+          <h1 className="main-title">Computerra</h1>
+          <p className="main-p">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit
+            molestias quos dolore assumenda suscipit magnam aliquam eum!
+            Quisquam, vitae sint?
+          </p>
+          <p className="main-p">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit
+          </p>
+        </Grid>
+        <Grid item xs={6} className="video-container">
+          <Box sx={{ color: "black" }} className="intro-video">
+            <video className="video" width="100%" height="100%" loop controls>
+              <source src={`${video}`} type="video/mp4" />
+            </video>
+          </Box>
+        </Grid>
+      </Grid>
+    </Box>
+  );
+}
