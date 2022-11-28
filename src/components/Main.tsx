95b608e154fcdd55852bfd139b1271b0421c013a
Denis Homoki <homoki.denisth@gmail.com>
2022-11-28 20:08:36 +0200

    problem fixed

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
-import { styled } from "@mui/material/styles";
+
 import Grid from "@mui/material/Grid";
-import Paper from "@mui/material/Paper";
+
 import Box from "@mui/material/Box";
 import video from "../assets/computerra.mp4";
 
